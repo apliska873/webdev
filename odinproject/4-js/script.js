3003f11bc99249e0e330885cdb1fbec005e298ea
austin <49465593+apliska873@users.noreply.github.com>
2026-04-29 21:10:07 -0400

    Rename lesson directories to have shorter names. Remove landing page mini project because I didn't feel like ever finishing it. Complete first javascript lesson.

---
+++ odinproject/4-js/script.js
@@ -0,0 +1,33 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName)
+console.log(lastName)
+
+let age = 11;
+console.log(age);
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+// pi = 10;   // will throw error
+console.log(pi);
+
+// assigment stuff
+console.log(23 + 97);
+console.log(1+2+3+4+5+6);
+console.log( (4 + 6 + 9) / 77);
+
+// variable playtime
+let a = 10;
+console.log(a);
+a = 897;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
